4fb54cd3cb44705ba12f1d9735d5e1a42612ba16
MinaNasrySci <mina-nasry@outlook.com>
2024-09-29 22:48:37 +0300

    Testing new code in pthreads_example_2.c file

---
+++ pthreads_example_2.c
@@ -0,0 +1,32 @@
+#include <pthread.h>
+#include <stdio.h>
+
+#define NUM_THREADS 4;
+
+
+void *threadFunc(void *pArg)
+{
+    int *p = (int *)pArg;
+    int myNum = *p;
+    printf("Thread number: %d\n", myNum);
+    return 0;
+}
+
+int main(void)
+{
+    int i;
+    pthread_t tid[NUM_THREADS];
+    pthread_attr_t attr;
+
+    for (i = 0; i < NUM_THREADS; i++)
+    {
+        pthread_create(&tid[i], NULL, threadFunc, &i)
+    }
+
+    for (i = 0; i < NUM_THREADS; i++)
+    {
+        pthread_join(tid[i], NULL);
+    }
+
+    return 0;
+}
