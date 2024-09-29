82bf8a4a48dd3402b9723d9db63ebe7eb2ebd929
MinaNasrySci <mina-nasry@outlook.com>
2024-09-29 20:43:09 +0300

    Adding initial files in PThreads folder

---
+++ pthreads.c
@@ -0,0 +1,29 @@
+#include <pthread.h>
+#include <stdio.h>
+
+#define NUM_THREADS 4
+
+void *hello(void *args)
+{
+    printf("Hello, thread!\n");
+    return 0;
+}
+
+int main(void)
+{
+    int i;
+
+    pthread_t tid[NUM_THREADS];
+
+    for (i = 0; i < NUM_THREADS; i++)
+    {
+        pthread_create(&tid[i], NULL, hello, NULL);
+    }
+
+    for (i = 0; i < NUM_THREADS; i++)
+    {
+        pthread_join(tid[i], NULL);
+    }
+
+    return 0;
+}
