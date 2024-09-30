485e331ba7f91c1deb3bc0aef78ff6d802f74328
MinaNasrySci <mina-nasry@outlook.com>
2024-09-30 19:07:55 +0300

    Updating pthreads_examples_2.c file

--- pthreads_example_2.c
+++ pthreads_example_2.c
@@ -8,7 +8,9 @@ void *threadFunc(void *pArg)
 {
     int *p = (int *)pArg;
     int myNum = *p;
+
     printf("Thread number: %d\n", myNum);
+
     return 0;
 }
 
@@ -16,7 +18,7 @@ int main(void)
 {
     int i;
     pthread_t tid[NUM_THREADS];
-    
+
     for (i = 0; i < NUM_THREADS; i++)
     {
         pthread_create(&tid[i], NULL, threadFunc, &i);
